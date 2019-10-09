caef5887d16699349644f44c78101d0eff5e26b5
hyunsik-321 <asbbbrp@kookmin.ac.kr>
2019-10-10 04:22:21 +0900

    20191605백현식 GUI프로그래밍 수정-최종본..뿌듯

--- assignment6_skel.py
+++ assignment6_skel.py
@@ -2,7 +2,7 @@ import pickle
 import sys
 from PyQt5.QtWidgets import (QWidget, QPushButton,
     QHBoxLayout, QVBoxLayout, QApplication, QLabel,
-    QComboBox, QTextEdit, QLineEdit, QGridLayout)
+    QComboBox, QTextEdit, QLineEdit)
 from PyQt5.QtCore import Qt
 
 
@@ -14,62 +14,112 @@ class ScoreDB(QWidget):
         self.dbfilename = 'assignment6.dat'
         self.scoredb = []
         self.readScoreDB()
-        self.showScoreDB()
+        self.showScoreDB(self.scoredb, 'Name')
 
-    def initUI(self) :
-        name = QLabel("Name :")
-        age = QLabel("Age :")
-        score = QLabel("Score :")
-        amount = QLabel("Amount :")
-        key = QLabel("Key :")
-        result = QLabel("<Result>")
-        addB = QPushButton("Add")
-        delB = QPushButton("Del")
-        findB = QPushButton("Find")
-        lncB = QPushButton("lnc")
-        showB = QPushButton("Show")
+    def initUI(self):
+        self.name = QLabel("Name :")
+        self.age = QLabel("Age :")
+        self.score = QLabel("Score :")
+        self.amount = QLabel("Amount :")
+        self.key = QLabel("Key :")
+        self.result = QLabel("<Result>")
+
+        self.nameEdit = QLineEdit()
+        self.ageEdit = QLineEdit()
+        self.scoreEdit = QLineEdit()
+        self.amountEdit = QLineEdit()
+        self.resultEdit = QTextEdit()
+        self.keyEdit = QComboBox()
+        self.keyEdit.addItem("Name")
+        self.keyEdit.addItem("Age")
+        self.keyEdit.addItem("Score")
+
+        self.showB = QPushButton("Show")
+        self.addB = QPushButton("Add")
+        self.delB = QPushButton("Del")
+        self.findB = QPushButton("Find")
+        self.incB = QPushButton("Inc")
 
 
-        nameEdit = QLineEdit()
-        ageEdit = QLineEdit()
-        scoreEdit = QLineEdit()
-        amountEdit = QLineEdit()
-        resultEdit = QTextEdit()
-        keyEdit = QComboBox()
+        self.showB.clicked.connect(self.showbuttonClicked)
+        self.addB.clicked.connect(self.addbuttonClicked)
+        self.delB.clicked.connect(self.delbuttonClicked)
+        self.findB.clicked.connect(self.findbuttonClicked)
+        self.incB.clicked.connect(self.incbuttonClicked)
 
-        grid = QGridLayout()
-        #grid.setSpacing()
+        hbox1 = QHBoxLayout()
+        hbox1.addWidget(self.name)
+        hbox1.addWidget(self.nameEdit)
+        hbox1.addWidget(self.age)
+        hbox1.addWidget(self.ageEdit)
+        hbox1.addWidget(self.score)
+        hbox1.addWidget(self.scoreEdit)
 
-        grid.addWidget(name, 1, 0)
-        grid.addWidget(nameEdit, 1, 1)
+        hbox2 = QHBoxLayout()
+        hbox2.addWidget(self.amount)
+        hbox2.addWidget(self.amountEdit)
+        hbox2.addStretch(1)
 
-        grid.addWidget(age, 1, 2)
-        grid.addWidget(ageEdit, 1, 3)
+        hbox3 = QHBoxLayout()
+        hbox3.addWidget(self.result)
+        hbox3.addStretch()
+        hbox3.addWidget(self.key)
+        hbox3.addWidget(self.keyEdit)
+        hbox3.addWidget(self.showB)
 
-        grid.addWidget(score, 1, 4)
-        grid.addWidget(scoreEdit, 1, 5)
+        v1box = QVBoxLayout()
+        v1box.addWidget(self.addB)
+        v1box.addWidget(self.delB)
+        v1box.addWidget(self.findB)
+        v1box.addWidget(self.incB)
 
-        grid.addWidget(amount, 2, 0)
-        grid.addWidget(amountEdit, 2, 1)
+        hbox4 = QHBoxLayout()
+        hbox4.addWidget(self.resultEdit)
+        hbox4.addLayout(v1box)
 
-        grid.addWidget(key, 3, 4)
-        grid.addWidget(keyEdit, 3, 5)
+        hbox = QVBoxLayout()
+        hbox.addLayout(hbox1)
+        hbox.addLayout(hbox2)
+        hbox.addLayout(hbox3)
+        hbox.addLayout(hbox4)
 
-        grid.addWidget(result, 3, 0)
-        grid.addWidget(resultEdit, 4, 0, 6, 5)
-
-        grid.addWidget(addB, 4, 5)
-        grid.addWidget(delB, 5, 5)
-        grid.addWidget(findB, 6, 5)
-        grid.addWidget(lncB, 7, 5)
-        grid.addWidget(showB, 8, 5)
-
-        self.setLayout(grid)
-
-        self.setGeometry(300, 300, 500, 600)
+        self.setLayout(hbox)
+        self.setGeometry(300, 300, 500, 250)
         self.setWindowTitle('Assignment6')
-
         self.show()
+
+    def showbuttonClicked(self):
+        self.showScoreDB(self.scoredb, self.keyEdit.currentText())
+
+    def addbuttonClicked(self):
+        try:
+            record = {"Name":self.nameEdit.text(), "Age":int(self.ageEdit.text()), "Score":int(self.scoreEdit.text())}
+            self.scoredb += [record]
+            self.showbuttonClicked()
+        except:
+            pass
+
+    def delbuttonClicked(self):
+        for p in self.scoredb[:]:
+            if p['Name'] == self.nameEdit.text():
+                self.scoredb.remove(p)
+        self.showbuttonClicked()
+
+
+    def findbuttonClicked(self):
+        self.resultEdit.setPlainText('')
+        for p in sorted(self.scoredb, key=lambda person: person[self.keyEdit.currentText()]):
+            if p['Name'] == self.nameEdit.text():
+                for attr in sorted(p):
+                    self.resultEdit.insertPlainText(
+                        attr + " = " + str(p[attr]) + ('\t\t' if type(p[attr]) == str and len(p[attr]) < 5 else '\t'))
+                self.resultEdit.insertPlainText('\n')
+
+    def incbuttonClicked(self):
+        for p in self.scoredb:
+            if p["Name"] == self.nameEdit.text():
+                p['Score'] += int(self.amountEdit.text())
+        self.showbuttonClicked()
 
     def closeEvent(self, event):
         self.writeScoreDB()
@@ -89,18 +139,20 @@ class ScoreDB(QWidget):
             pass
         fH.close()
 
-
     # write the data into person db
     def writeScoreDB(self):
         fH = open(self.dbfilename, 'wb')
         pickle.dump(self.scoredb, fH)
         fH.close()
 
-    def showScoreDB(self):
-        pass
+    def showScoreDB(self, db, keyname):
+        self.resultEdit.setPlainText('')
+        for p in sorted(db, key = lambda person : person[keyname]):
+            for attr in sorted(p):
+                self.resultEdit.insertPlainText(attr + " = " + str(p[attr]) + ('\t\t' if type(p[attr]) == str and len(p[attr]) < 5 else '\t'))
+            self.resultEdit.insertPlainText('\n')
 
-
-if __name__ == '__main__':    
+if __name__ == '__main__' :
     app = QApplication(sys.argv)
     ex = ScoreDB()
     sys.exit(app.exec_())
